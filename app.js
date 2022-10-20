591ab2c93baa6124457b2e3f477871e179ef0c34
João P Porto <jpportodev@gmail.com>
2022-10-20 10:46:03 -0300

    mongo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+
+const checkListRouter = require('./src/routes/checklist');
+
+require('./config/database.js');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/checklists',checkListRouter);
+
+app.listen(3000, ()=>{
+    console.log('Servidor foi iniciado')});
---
+++ src/routes/models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const checklist = require('./checklist');
+
+const checklistSchema = mongoose.Schema({
+    name:{type:String, 
+        required:true},
+    done:{type:Boolean, 
+        default:false},
+    checklist:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Checklist',
+        required: true
+    }
+});
+
+module.exports=mongoose.model('Task', checklistSchema);
